edab3a8180a0a6149ffc13be03abd47d46575484
Joel Matos <matosj192@gmail.com>
2016-11-13 13:34:26 -0500

    add crud to my capstone_jr app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,44 @@
 class ProductsController < ApplicationController
-	def info
+	def index
 		@products = Product.all
 		render 'product.html.erb'
 	end
+	
+	def show
+		# p params
+		@product = Product.find_by(id: params[:id] )
+		render 'show.html.erb'
+	end
+
+	def new
+		render 'new.html.erb'
+	end
+
+	def create
+	#make a new recipe from the params
+	@product = Product.new({name: params["name"], price: params["price"]})
+	 @product.save
+		render 'create.html.erb'
+	end
+
+	def edit 
+		@product = Product.find_by(id: params[:id])
+		render 'edit.html.erb'
+		@product.update
+	end
+
+	def update
+		@product = Product.find_by( id: params["id"])
+		@product.update(name: params["name"], price: params["price"])
+		render 'update.html.erb'
+	end
+
+	def destroy
+		#grab the right product
+		@product = Product.find_by( id: params["id"])
+		#kill it 
+		@product.destroy
+		render 'destory.html.erb'
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/them_thangs' => 'products#info'
+  get '/products' => 'products#index'
+  get 'products/new' => 'products#new'
+  get '/products/:id' => 'products#show'
+  post 'products' => 'products#create'
+  get 'products/:id/edit' => 'products#edit'
+  patch 'products/:id' => 'products#update'
+  delete 'products/:id' =>
+  'products#destroy'
 end
